3fd25afad014f01ec2a9e273187195b1f6b921ff
yahsuanlinus <savannahwang75@gmail.com>
2022-01-25 07:39:08 -0500

    finished

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
 const form = document.querySelector('form')
-console.log(form)
+const ul = document.querySelector('ul')
+const input = form.querySelector('input')
+
+ul.addEventListener('click', (ev)=> {
+	const li = ev.target.parentNode
+	const action = ev.target.getAttribute('data-action')
+	const span = li.querySelector('span')
+	const value = span.innerHTML
+
+	if(ev.target.tagName === 'BUTTON') {
+		if(action === 'delete') {
+			li.parentNode.removeChild(li)
+		} 
+		if(action === 'add') {
+			span.innerHTML = value*1 + 1
+		} 
+		if(action === 'minus'){
+			span.innerHTML = value*1  - 1
+		}
+	}
+}, false)
+
+form.addEventListener('submit', (ev) => {
+	ev.preventDefault()
+	const value = input.value
+	if(isNaN(value)) {
+		return alert( 'Numbers Only')  
+	}
+	const li = document.createElement('li')
+	li.innerHTML = `
+		<span>${value}</span>
+		<button data-action="delete">x</button>
+		<button data-action="add">+</button>
+		<button data-action="minus">-</button>
+	`
+	ul.appendChild(li)
+	input.value = ''
+} ,false)
